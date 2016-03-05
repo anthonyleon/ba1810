637e82637d447a3ca0ede921ceb9f0074fd7062f
san3091 <san3091@gmail.com>
2016-03-05 15:10:05 -0500

    added confirmed and sonfirm token columns to company, nested member email confirm inside company routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,12 @@ Rails.application.routes.draw do
     resources :bids
   end
 
-  resources :companies
+  resources :companies do
+    member do 
+      get :confirm_email
+    end
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
