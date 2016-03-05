0392a5bd8e269ad80c36398d0e8fee13bd0d28f1
san3091 <san3091@gmail.com>
2016-03-05 12:23:29 -0500

    add click highlight on inv pt new

--- app/assets/javascripts/bids.js
+++ app/assets/javascripts/bids.js
@@ -1,9 +1,11 @@
 $(document).on('ready page: load', function() {
   console.log('ready');
     $('table tbody tr').click(function(){
+      console.log(this);
+      $(this).addClass('active');
       console.log('click');
-        var hiddenField = $('#hiddeninv').val();
-        var mydata = $('data-inventory-part-id').data();
-          console.log(hiddenField);
-        });
+      var hiddenField = $('#hiddeninv').val();
+      var mydata = $('data-inventory-part-id').data();
+      console.log(hiddenField);
     });
+});
