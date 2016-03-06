8e6d95419800275fc46f330ac2fdc852bdb78f1d
Nicholas Rodriguez <nrodriguez3179@gmail.com>
2016-03-06 17:22:02 -0500

    Implemented stripe with testing production key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,6 @@
-require "stripe"
-Stripe.api_key = "sk_test_JaFXqkUnZAlFA2jNazzMGtlg"
-
-
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => ENV['GHSQ1SVwsKr7xQcMKMbYGh7h'],
+  :secret_key      => ENV['JaFXqkUnZAlFA2jNazzMGtlg']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:publishable_key]
