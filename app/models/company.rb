d54834b2bf878179a1271a764ad63bee78fc7579
san3091 <san3091@gmail.com>
2016-03-06 15:25:27 -0500

    email confirmation done

--- app/models/company.rb
+++ app/models/company.rb
@@ -9,17 +9,17 @@ class Company < ActiveRecord::Base
 
   before_create :confirmation_token
 
+  def email_activate
+    self.email_confirmed = true
+    self.confirm_token = nil
+    save!(validate: false)
+  end
+
   private
   
   def confirmation_token
     if self.confirm_token.blank?
       self.confirm_token = SecureRandom.urlsafe_base64.to_s
     end
-  end
-
-  def email_activate
-    self.email_confirmed = true
-    self.confirm_token = nil
-    save!(validate: false)
   end
 end
