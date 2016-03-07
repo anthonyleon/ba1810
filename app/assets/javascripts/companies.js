1003a53fc3d2aaf286ae3c14f7dcaaf9768f944e
san3091 <san3091@gmail.com>
2016-03-07 15:52:12 -0500

    company show styles done

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -1,27 +1,30 @@
 
-
 $(document).on('ready page:load', function(){
+  $('#dashboard-auctions>div:visible').hide();
+
   $('.nav-option').click(function() {
     $(this).addClass('active');
+    $(this).siblings('.nav-option').removeClass('active');
+
     var num = $(this).attr('id');
-    $(this).siblings('.nav-option').each(function() {
-      $(this).removeClass('active');
-    });
-    $('#dashboard-auctions>div:visible').fadeOut(function() {
-      switch(num) {
-        case "2":
-          $('#buyer-auctions').fadeIn();
-          break;
-        case "3":
-          $('#supplier-auctions').fadeIn();
-          break;
-        case "4":
-          $('#possible-auctions').fadeIn();
-          break;
-        case "5":
-          $('#inactive-auctions').fadeIn();
-          break;
-      }
-    });
+    $('#dashboard-auctions>div').hide();
+    switch(num) {
+      case "2":
+        $('#buyer-auctions').fadeIn();
+        break;
+      case "3":
+        $('#supplier-auctions').fadeIn();
+        break;
+      case "4":
+        $('#possible-auctions').fadeIn();
+        break;
+      case "5":
+        $('#inactive-auctions').fadeIn();
+        break;
+    };
   });
+
+  // default tab is 2
+  $('.nav-option[id="2"').click();
 });
+
