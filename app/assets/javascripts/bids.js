5828504304a28b9000c69a7df6d849a5ae6cad66
san3091 <san3091@gmail.com>
2016-03-08 13:00:17 -0500

    fix javascript table highlight active class

--- app/assets/javascripts/bids.js
+++ app/assets/javascripts/bids.js
@@ -1,11 +1,12 @@
 $(document).on('ready page: load', function() {
   console.log('ready');
     $('table tbody tr').click(function(){
-      console.log(this);
+
+      $(this).parent().children().removeClass('active');
       $(this).addClass('active');
-      console.log('click');
+
       var hiddenField = $('#hiddeninv').val();
       var mydata = $('data-inventory-part-id').data();
-      console.log(hiddenField);
+
     });
 });
