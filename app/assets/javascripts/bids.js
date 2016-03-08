d94f56710ab246cff41e40b4cb3214663311392c
Anthony Leon <anthonyleon@me.com>
2016-03-08 15:25:20 -0500

    updated mailer host, changed bids to toggle class

--- app/assets/javascripts/bids.js
+++ app/assets/javascripts/bids.js
@@ -2,8 +2,7 @@ $(document).on('ready page: load', function() {
   console.log('ready');
     $('table tbody tr').click(function(){
 
-      $(this).parent().children().removeClass('active');
-      $(this).addClass('active');
+      $(this).toggleClass('active');
 
       var hiddenField = $('#hiddeninv').val();
       var mydata = $('data-inventory-part-id').data();
