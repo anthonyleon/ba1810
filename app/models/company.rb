9e1664cb6179e9eee5f0495b661432a054bfd2ca
Anthony Leon <anthonyleon@me.com>
2016-06-28 12:39:36 -0400

    generate scaffold for aircraft, create associations, foreign key

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,7 @@ class Company < ActiveRecord::Base
   has_many :auctions, dependent: :destroy
   has_many :bids
   has_many :inventory_parts
+  has_many :aircrafts
   validates :password, presence: true, length: { minimum: 8 }
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
