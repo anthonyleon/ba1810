2dab1f0b2f947fdba450dc0a21187fd1fbb0c23d
Gabo Martinez <gabmart02@gmail.com>
2016-07-12 17:40:28 -0400

    fixing notifications and part match for current opportunities

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,12 +1,18 @@
 class NotificationsController < ApplicationController
   before_action :unread, only: [:index]
+  after_action :mark_as_read, only: [:index]
   def index
-  	
+  	@notifications = current_user.notifications
   end
 
 
+  private
 
   def unread
-  	@notifications = current_user.notifications.where(unread?: true)
+  	@notifications = current_user.notifications.where(read?: false)
+  end
+
+  def mark_as_read
+  	@notifications.update_all(read?: true)
   end
 end
