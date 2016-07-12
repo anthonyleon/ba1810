6b257078c407604e2ab9601a5031063b3af6acf7
Nicholas Rodriguez <nrodriguez3179@gmail.com>
2016-07-12 10:05:32 -0400

    test written to upload single line of inventory

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -5,10 +5,17 @@ feature "User has the ability to sign out", :javascript => true do
 		sign_in
 	end
 
-	scenario "User signs outs from the dashboard" do
-
-		find_link('Log out').click
-
-		save_and_open_page
+	scenario "User signs out by clicking the buttom on the TOP NAVBAR" do
+		within('ul.right.hide-on-med-and-down') do
+			find_link('Log out').click
+		end
 	end
+
+	scenario "User signs outs by clicking the button on the SIDE NAVBAR" do
+		within('ul#mobile-demo.side-nav.fixed') do
+			find_link('Log out').click
+		end
+	end
+
+
 end
