901b95bf714010271fe5dbfc08b76dba0a98b536
Nicholas Rodriguez <nrodriguez3179@gmail.com>
2016-08-05 13:46:52 -0400

    Added States List

--- spec/helpers/sign_in.rb
+++ spec/helpers/sign_in.rb
@@ -1,9 +1,9 @@
 module SignInHelper 
 	def sign_in
-		@company = Company.create(name: "Hellooooo", email: 'bugger1@email.com', password: 'password')
+		@company = Company.create(name: "Hellooooo", email: 'bugger1@email.com', password: 'password1', user_phone: '305-878-3720')
 		visit login_path
 		fill_in('login[email]', with: 'bugger1@email.com')
-		fill_in('login[password]', with: 'password')
+		fill_in('login[password]', with: 'password1')
 		click_button("Log In")
 		visit("/companies/#{@company.id}/confirm_email.#{@company.confirm_token}")
 	end
