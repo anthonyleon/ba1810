b2ec756a9b10160253555bf5fe20c4c1c03b5f25
Henry Arbolaez <henryarbolaez@gmail.com>
2016-08-22 18:21:55 -0400

    Added styles in final

--- db/schema.rb
+++ db/schema.rb
@@ -229,6 +229,7 @@ ActiveRecord::Schema.define(version: 20160817210804) do
 
   create_table "transactions", force: :cascade do |t|
     t.string   "order_id"
+    t.integer  "auction_id"
     t.integer  "inventory_part_id"
     t.string   "po_num"
     t.string   "invoice_num"
