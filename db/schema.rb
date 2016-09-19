da217527bdc6f7ff0bbdf4ce0b9a0f996b6f2041
Nicholas Rodriguez <nrodriguez3179@gmail.com>
2016-09-19 11:17:29 -0400

    Had to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20160914204444) do
     t.datetime "updated_at",        null: false
     t.string   "invoice_num"
     t.integer  "transaction_id"
+    t.decimal  "total_amount"
     t.decimal  "part_price"
     t.decimal  "est_shipping_cost"
   end
@@ -135,6 +136,22 @@ ActiveRecord::Schema.define(version: 20160914204444) do
   end
 
   add_index "company_docs", ["company_id"], name: "index_company_docs_on_company_id", using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "documents", force: :cascade do |t|
     t.string   "name"
