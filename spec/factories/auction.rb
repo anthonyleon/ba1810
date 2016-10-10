0a2fd3aa69a9516f6c388945b28052b646e52c6b
Gabo Martinez <gabmart02@gmail.com>
2016-10-10 14:02:27 -0400

    move :javascript tags one indent to the right so they are within %html tag, create and pass first test

--- spec/factories/auction.rb
+++ spec/factories/auction.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 	factory :auction do
 		part_num	 						"9000000-20004"
 		active	 							true
-		condition	 						2
+		condition	 						[:as_removed]
 		destination_address		Faker::Address.street_address
 		destination_zip	 			Faker::Address.zip
 		destination_city	 		Faker::Address.city_suffix
--- spec/features/creating_auction_spec.rb
+++ spec/features/creating_auction_spec.rb
@@ -1,32 +1,15 @@
 require 'rails_helper'
 
-feature "creating an auction", :javascript => true do
-	before do 
+feature "Creating an auction" do
+
+	scenario "allow a company to create an auction" do
+		auction_part = create(:auction_part)
+		p auction_part.auction
 		sign_in
-		auction_part_match
-	end
+		visit auction_path(auction_part.auction)
 
-
-	scenario "allow a company to search for a part" do
-
-		find_link('Part Search').click
-		fill_in 'Part number', :with => "8063-215"
-		check ("New")
-
-		click_button "Submit"
-
-		within('ul#mobile-demo.side-nav.fixed') do 
-			find_link('Dashboard').click
-		end
-
-		within('ul#mobile-demo.side-nav.fixed') do
-			find_link('Current Auctions').click
-		end
-
-		find_link('Show').click
-
-
-		save_and_open_page
+		expect(page).to have_content("9000000-20004")
+		expect(page).to have_content("As removed")
 	end
 end
 
