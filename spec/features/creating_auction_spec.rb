f6a474baf86024b53e54a481b9a405afa2666d41
Gabo Martinez <gabmart02@gmail.com>
2016-10-10 17:01:04 -0400

    create auction test and signing in.. and finishing material cert pdf formatting

--- spec/features/creating_auction_spec.rb
+++ spec/features/creating_auction_spec.rb
@@ -2,14 +2,29 @@ require 'rails_helper'
 
 feature "Creating an auction" do
 
-	scenario "allow a company to create an auction" do
+	scenario "auction available in user's auctions page" do
 		auction_part = create(:auction_part)
-		p auction_part.auction
+		p @auction = auction_part.auction
+		p @company = @auction.company
 		sign_in
 		visit auction_path(auction_part.auction)
 
 		expect(page).to have_content("9000000-20004")
 		expect(page).to have_content("As removed")
+		expect(@auction).to eql(@company.auctions.last)
+
+
+
+		bid = create(:bid)
+		@company = bid.company
+
+		sign_in
+
+		expect(@auction.bids).to include(bid)
+	end
+
+	scenario "Creating a bid" do
+		
 	end
 end
 
