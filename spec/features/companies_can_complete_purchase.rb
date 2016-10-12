4ead408a55ae2a815ab5d69682effae0a2614bef
Gabo Martinez <gabmart02@gmail.com>
2016-10-12 13:57:41 -0400

    Helper to fill in Checkout part minus the purchase order number

--- spec/features/companies_can_complete_purchase.rb
+++ spec/features/companies_can_complete_purchase.rb
@@ -12,14 +12,24 @@ feature 'Companies can complete checkout process' do
                               inventory_part: inventory_part) }
 
   context 'Checkout' do
+    before do
+      sign_in buyer
+      visit auction_path(auction)
+      click_button 'Checkout'
+    end
+
   	it ' allow buyer to choose a part and checkout.' do
-  		sign_in buyer
-  		visit auction_path(auction)
-  		expect(page).to have_content seller.name
-  		click_button 'Checkout'
-  		save_and_open_page
-  		
+  		expect(page).to have_content 'Please confirm shipping address' 		
   	end
+
+    it ' checkout should have destination fields filled' do
+      expect(page).to have_field('auction[destination_company]', auction.destination_address)
+      expect(page).to have_field('auction[destination_address]', with: auction.destination_address)
+      expect(page).to have_field('auction[destination_city]', with: auction.destination_city)
+      expect(page).to have_field('auction[destination_state]', auction.destination_state)
+      expect(page).to have_field('auction[destination_zip]', with: auction.destination_zip)
+      expect(page).to have_field('auction[destination_country]', with: auction.destination_country)
+    end
 
   	
   end
