50a31cf96e21e5f1d394f57f3d1c1a6dd9868f50
Nicholas Rodriguez <nrodriguez3179@gmail.com>
2016-10-17 13:05:18 -0400

    Fixed document uploader to make sure that users can not upload an empty document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,7 @@
 class Document < ActiveRecord::Base
   mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
   validates :name, presence: true # Make sure the owner's name is present.
+  validates :attachment, presence: true
   belongs_to :inventory_part
   belongs_to :company
   belongs_to :aircraft
