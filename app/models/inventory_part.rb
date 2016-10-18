23f4dd1710be575a990a75cdeb81fdd99d7383af
Gabo Martinez <gabmart02@gmail.com>
2016-10-18 13:01:57 -0400

    add validations to excel upload of parts (make sure serial number unique and that part exists), and put back in inventory_parts form

--- app/models/inventory_part.rb
+++ app/models/inventory_part.rb
@@ -5,13 +5,15 @@ class InventoryPart < ActiveRecord::Base
   belongs_to :company
   belongs_to :part
   has_one :tx, class_name: "Transaction", foreign_key: "transaction_id"
-  has_many :bids
+  has_many :bids, dependent: :destroy
   has_many :documents, dependent: :destroy
 
   enum condition: [:recent, :overhaul, :as_removed, :serviceable, :non_serviceable, :scrap]
 
   # class methods for importing xls, csv files using Roo
-  def self.import(file, current_user)
+  def self.import(file, company)
+    array = []
+    counter = 0
 	  spreadsheet = open_spreadsheet(file)
   	  header = spreadsheet.row(1)
   	  (2..spreadsheet.last_row).each do |i|
@@ -20,14 +22,22 @@ class InventoryPart < ActiveRecord::Base
   	  	part.attributes = row.to_hash.slice(*row.to_hash.keys)
         part.update(part.attributes)
         @part_match = Part.find_by(part_num: part.part_num)
-        if @part_match
+        if @part_match 
           build_inv_part(@part_match, part)
-          part.company = current_user
-        	part.save! 
+          part.company = company
+        	if company.inventory_parts.exists?(serial_num: part.attributes["serial_num"])
+            counter += 1
+            array[0] = counter
+          else
+            part.save!
+          end
         else
-          break
+          array[1] = part.part_num
+          return array
         end
+
       end
+      return array
   end
 
   def self.build_inv_part part_match, inventory_part
