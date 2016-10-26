1fa8ec89db8ea86ed8640677ae470d6bb0c6b538
Gabo Martinez <gabmart02@gmail.com>
2016-10-26 13:44:08 -0400

    add that a user must select a payout preference before creating a bid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019153311) do
+ActiveRecord::Schema.define(version: 20161026154948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20161019153311) do
     t.string   "inc_country"
     t.string   "inc_state"
     t.integer  "business_type"
+    t.boolean  "payout_selected"
   end
 
   add_index "companies", ["email"], name: "index_companies_on_email", unique: true, using: :btree
