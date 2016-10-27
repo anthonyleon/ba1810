861ef5243158cede6c4af11a12787497f448c1b4
Gabo Martinez <gabmart02@gmail.com>
2016-10-27 16:31:00 -0400

    changing dashboard

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -13,6 +13,10 @@ class CompaniesController < ApplicationController
     @company = current_user
     yahoo_client = YahooFinance::Client.new
     @data = yahoo_client.quotes(["AER", "AYR", "FLY", "AL", "ACY", "WLFC"], [:symbol, :name, :ask, :change, :change_in_percent, :market_capitalization])
+    @opportunity_count = Auction.get_sales_opportunities(current_user).count
+    @inventory_count = current_user.inventory_parts.count
+    @bids_count = current_user.bids.count
+    @notifications = current_user.notifications.order(created_at: :desc).limit(10)
   end
 
   def new
