e1ae2f7da600d3037c07d8cb70559fa2c617e9c7
Gabo Martinez <gabmart02@gmail.com>
2016-11-01 13:11:10 -0400

    add and style error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,9 @@
 class ErrorsController < ApplicationController
   def not_found
-  	render(status: 404)
+  	render(status: 404, layout: "/commons/_error_navbar.html.haml")
   end
 
   def internal_server_error
-  	render(status: 500)
+  	render(status: 500, layout: "/commons/_error_navbar.html.haml")
   end
 end
