681df927f5e009d7d60e7972621f991b8054f8fb
Gabo Martinez <gabmart02@gmail.com>
2016-11-03 12:15:29 -0400

    move default email host to development and production

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,7 @@ module BidAero
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_dispatch.default_headers = {'X-Frame-Options' => 'ALLOWALL'}
-    # set host for production email
-    config.action_mailer.default_url_options = { host: "localhost:3000" }
+
     config.active_job.queue_adapter = :sidekiq
 
     # secret keys
