d009c7a999fe2e705efa898d705ac42d84558594
Gabo Martinez <gabmart02@gmail.com>
2016-11-03 12:22:41 -0400

    add email action when a user logs on. Bid Aero support should be notified.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -44,7 +44,8 @@ class CompaniesController < ApplicationController
     @company = Company.new(company_params)
     respond_to do |format|
       if @company.save
-          ## need to make validations so that errors are note received from armor payments (testing purposes)
+          ## need to make validations so that errors are not received from armor payments (testing purposes)
+        AdminMailer.new_register(@company)
         CompanyMailer.registration_confirm(@company).deliver
         # session[:company_id] = @company.id
         format.html { redirect_to root_path, notice: 'Please confirm your email address to complete registration.' }
