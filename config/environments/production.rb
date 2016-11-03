25bf78db226e94f8aee58c0264f540ecf61fea97
Gabo Martinez <gabmart02@gmail.com>
2016-11-03 12:15:29 -0400

    move default email host to development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "shrouded-bastion-88554.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "bid-aero.herokuapp.com" }
 
 
 
