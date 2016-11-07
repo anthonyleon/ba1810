103afaa4dc87ddb90a7f65a2a7562c663726f599
Gabo Martinez <gabmart02@gmail.com>
2016-11-07 17:31:49 -0500

    add HTTParty to avref class

--- app/models/av_ref_api.rb
+++ app/models/av_ref_api.rb
@@ -2,10 +2,14 @@ class AvRefApi
 	# The general format of the request url is: http://www.avrefweb.com/Service/<License Key>/<Request Type>/<Search Value>
 	# example call http://avrefweb.com/Service/e85c0e58fb89427cb47948cba2945a7d/partnum/A07D32
 
-	def part_num_check(part_num)
+	def self.part_num_check(part_num)
 		request_type = 'partnum'
 		search_value = part_num
-		p response = HTTParty.get("http://www.avrefweb.com/Service/#{ENV['AV_REF_KEY']}/#{request_type}/#{search_value}")
+		response = HTTParty.get("http://www.avrefweb.com/Service/#{ENV['AV_REF_KEY']}/#{request_type}/#{search_value}")
+		# I don't know if this is the best check
+		## bad response response.body.to_json[2] == "]"
+		## good response response.body.to_json[2] == "{"
+
 	end
 
 end
