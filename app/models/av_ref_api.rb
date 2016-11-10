1b47025930ea249fdf021314465084115b86d7da
Gabo Martinez <gabmart02@gmail.com>
2016-11-10 12:22:20 -0500

    debug heroku

--- app/models/av_ref_api.rb
+++ app/models/av_ref_api.rb
@@ -17,6 +17,8 @@ class AvRefApi
 			part_match_found = !response.empty?		
 			if part_match_found 
 				response[0]["MfgPrice"].slice!("$")
+				p response
+				p "*****" * 80
 
 				part_match_found = {part_num: response[0]["PartNum"], description: response[0]["Description"], 
 					manufacturer: response[0]["Manufacturer"], manufacturer_price: response[0]["MfgPrice"].to_d }
