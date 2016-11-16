aa1c1282ffff332d36059ca9a3f02d920a31172c
Gabo Martinez <gabmart02@gmail.com>
2016-11-16 11:32:24 -0500

    rearrange js required in landing and application .js

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,7 +1,6 @@
-//= require jquery
+// took out requre jquery
 //= require jquery_ujs
 //= require sweetalert
 //= require jquery.turbolinks
 //= require bootstrap.min
 //= require_tree ./landing
-//= require maskmoney
