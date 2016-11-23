5ca1b3c9a4148c743193d664953294ea87c1371e
Gabo Martinez <gabmart02@gmail.com>
2016-11-23 15:33:48 -0500

    aircraft and engine redirect

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -23,7 +23,7 @@ class AircraftsController < ApplicationController
 
     respond_to do |format|
       if @aircraft.save
-        format.html { redirect_to @aircraft, notice: 'Aircraft was successfully created.' }
+        format.html { redirect_to aircrafts_path, notice: 'Aircraft was successfully created.' }
         format.json { render :show, status: :created, location: @aircraft }
       else
         format.html { render :new }
