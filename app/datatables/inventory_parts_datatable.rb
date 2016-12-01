f029c9449bfde408a22b3a2112eb21868c5376e6
Gabo Martinez <gabmart02@gmail.com>
2016-12-01 16:11:29 -0500

    rails cast modifications to datatables

--- app/datatables/inventory_parts_datatable.rb
+++ app/datatables/inventory_parts_datatable.rb
@@ -22,9 +22,10 @@ private
     inventory_parts.map do |part|
       [
         link_to(part.part_num, part),
-        (part.condition),
-        (part.created_at.strftime("%B %e, %Y")),
-        part.manufacturer
+        (part.description),
+        (part.serial_num || "N/A"),
+        (AssetDecorator.rename(part, part.condition)),
+        ((part.manufacturer) || "N/A")
       ]
     end
   end
@@ -36,26 +37,28 @@ private
   def fetch_inventory_parts
     inventory_parts = @company.inventory_parts.order("#{sort_column} #{sort_direction}")
     inventory_parts = inventory_parts.page(page).per_page(per_page)
-    if params[:sSearch].present?
-      inventory_parts = inventory_parts.where("part_num like :search or category like :search", search: "%#{params[:sSearch]}%")
+    if params[:search].present?
+
+      inventory_parts = inventory_parts.where("part_num like :search", search: "%#{params[:search]}%")
+      p params[:search]
     end
     inventory_parts
   end
 
   def page
-    params[:iDisplayStart].to_i/per_page + 1
+    params[:start].to_i/per_page + 1
   end
 
   def per_page
-    params[:iDisplayLength].to_i > 0 ? params[:iDisplayLength].to_i : 10
+    params[:length].to_i > 0 ? params[:length].to_i : 10
   end
 
   def sort_column
-    columns = %w[part_num category released_on price]
-    columns[params[:iSortCol_0].to_i]
+    columns = %w[part_num description serial_num condition manufacturer]
+    columns[params[:order]["0"][:column].to_i]
   end
 
   def sort_direction
-    params[:sSortDir_0] == "desc" ? "desc" : "asc"
+    params[:order]["0"][:dir] == "desc" ? "desc" : "asc"
   end
 end
