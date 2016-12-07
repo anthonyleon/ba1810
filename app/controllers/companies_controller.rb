fa64f4e79847cc173da7bef7b401a436198330b6
Gabo Martinez <gabmart02@gmail.com>
2016-12-07 15:16:39 -0500

    turning off registration_confirm email for now

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -40,7 +40,7 @@ class CompaniesController < ApplicationController
       if @company.save
           ## need to make validations so that errors are not received from armor payments (testing purposes)
         AdminMailer.new_register(@company).deliver
-        CompanyMailer.registration_confirm(@company).deliver
+        # CompanyMailer.registration_confirm(@company).deliver
         # session[:company_id] = @company.id
         format.html { redirect_to root_path, notice: 'Please confirm your email address to complete registration.' }
         format.json { render :show, status: :created, location: @company }
