850317f44d1d59f7202c611cb5bbb02babc1c128
Gabo Martinez <gabmart02@gmail.com>
2017-01-30 10:54:03 -0500

    add quantity to auctions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207203359) do
+ActiveRecord::Schema.define(version: 20170130154835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20161207203359) do
     t.date     "required_date"
     t.string   "destination_company"
     t.string   "cycles"
+    t.integer  "quantity"
   end
 
   add_index "auctions", ["company_id"], name: "index_auctions_on_company_id", using: :btree
