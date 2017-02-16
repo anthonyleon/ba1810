6e380651cc41b7212314b00e85a27ed8c1313e7a
Gabo Martinez <gabmart02@gmail.com>
2017-02-16 17:00:13 -0500

    return Procfile to previous state, and add config for client and server for InventoryUploadWorker

--- app/workers/inventory_upload_worker.rb
+++ app/workers/inventory_upload_worker.rb
@@ -2,6 +2,14 @@ class InventoryUploadWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"], network_timeout: 5 }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"], network_timeout: 5 }
+  end
+
   def perform(file_path, company_id)
     CsvImport.csv_import(file_path, Company.find(company_id))
   end
