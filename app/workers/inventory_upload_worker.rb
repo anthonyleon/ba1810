a1b19a7d83411fca6b96cb529b57ddc14a452349
Gabo Martinez <gabmart02@gmail.com>
2017-02-16 12:44:22 -0500

    make retry false for sidekiq

--- app/workers/inventory_upload_worker.rb
+++ app/workers/inventory_upload_worker.rb
@@ -1,5 +1,6 @@
 class InventoryUploadWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform(file_path, company_id)
     CsvImport.csv_import(file_path, Company.find(company_id))
