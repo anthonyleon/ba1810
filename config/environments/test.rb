912db270069ac904a9dfa34f988aa8d6533203f5
Gabo Martinez <gabmart02@gmail.com>
2017-03-09 21:18:39 -0500

    adds armor modal url for testing environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  config.armor_modal_url = "https://sandbox.armorpayments.com/assets/js/modal.min.js"
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
