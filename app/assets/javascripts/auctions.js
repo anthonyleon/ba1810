95c1a4574b8825cf203e910fcdcc59ee3a119aff
Gabo Martinez <gabmart02@gmail.com>
2017-03-28 15:08:13 -0400

    reorganizes the auction#new form and adds the dynamic field for adding suppliers to invite to auction

--- app/assets/javascripts/auctions.js
+++ app/assets/javascripts/auctions.js
@@ -5,7 +5,6 @@ $(document).on('ready page:load', function() {
 	// 	$('.auction-destination').trigger("reset");
 	// });
 
-
 	$('.card-edit-submit').click(function(){
 		$('.confirm-address-bubble').removeClass('active');
 		$('.confirm-address-bubble').addClass('visited');
@@ -14,5 +13,21 @@ $(document).on('ready page:load', function() {
 	$('.card-edit-remove').click(function(){
 		$('.card.destination-address.large').removeClass('large');
     //this method increases the height to 72px
-  });
+  	});
+
+  	$('.edit-address').click(function(){
+  		$('.edit-shipping').show(350);
+  	});
+
+  	$('#add-supplier-circle').click(function(){
+  		$('.supplier-field').append($('.container.add-new-supplier').html);
+  	});
+
+	$("#addNewSupplier").on("click", function() {
+	  $("#add-new-supplier").append($("#new-supplier-partial").html());
+	});
+
+	this.removeSupplier = function(element) {
+  		return element.parent().remove();
+	};
 });
