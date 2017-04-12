81e3e1d1304d36524ff57fd0068b00f0c67773b8
Gabo Martinez <gabmart02@gmail.com>
2017-04-12 17:37:13 -0400

    adds quantity to bids

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -6,7 +6,7 @@ class Bid < ActiveRecord::Base
   has_one :tx, class_name: "Transaction", dependent: :destroy
   has_many :notifications, dependent: :destroy
 
-  validates :inventory_part, presence: true
+  # validates :inventory_part, presence: true
   validates :part_price, presence: true
   validates :est_shipping_cost, presence: true
   # before_save :strip_symbols
