0ae943aff9e1e47bdec5bf24aa4e6f66eb1cc3f8
Gabo Martinez <gabmart02@gmail.com>
2017-05-18 17:33:36 -0400

    inventory upload modal in current opportunities

--- app/assets/javascripts/inventory_parts.js
+++ app/assets/javascripts/inventory_parts.js
@@ -6,5 +6,13 @@ $(document).on('ready page:load', function() {
         confirmButtonColor: "#2196F3",
         type: "info"
     });
-});
+  });
+  $('#sweet_opportunities').on('click', function() {
+    swal({
+        title: "Inventory Upload",
+        text: "Keep your opportunities current! \n Send us an e-mail with your updated inventory at \n\nsupport@bid.aero\n",
+        confirmButtonColor: "#2196F3",
+        type: "info"
+    });
+  });
 });
