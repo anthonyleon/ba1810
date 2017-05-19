a4d912297eca88e8a817bf63a4678882587cc5a6
Gabo Martinez <gabmart02@gmail.com>
2017-05-19 15:15:30 -0400

    fixes the way bid_aero_suppliers are being shown on the auctions/show page

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -3,7 +3,10 @@ class AuctionsController < ApplicationController
   before_action :set_bid_and_auction, only: [:purchase_confirmation, :purchase]
 
   def index
+    @myquotes_count = current_user.bids.to_a.uniq.count { |b| b.auction_id }
     @owned_auctions = current_user.auctions.where(active: true).where(project: nil)
+    @pending_purchases_count = Transaction.where(buyer_id: current_user.id, complete: false).count
+    @purchases_count = Transaction.where(buyer_id: current_user.id, complete: true).count
   end
 
   def show
@@ -11,10 +14,10 @@ class AuctionsController < ApplicationController
     ## test to make sure that the bids from a temp user and a bid aero supplier are separated properly
     @auction_invitees = Company.find_invitees(@auction.invitees)
 
-    
+
     @invited_suppliers_bids = @auction.bids.joins(:company).merge(@auction_invitees)
-    @auction_invitees.empty? ? @bid_aero_suppliers_bids = @auction.bids.joins(:company) : @bid_aero_suppliers_bids = 
-      @auction.bids.joins(:company).where("companies.id != ?", @auction_invitees.pluck(:id))
+    @auction_invitees.empty? ? @bid_aero_suppliers_bids = @auction.bids.joins(:company) : @bid_aero_suppliers_bids =
+      @auction.bids.joins(:company).where.not(companies: {id: @auction_invitees.pluck(:id)})
   end
 
   def auction_invites
@@ -45,7 +48,7 @@ class AuctionsController < ApplicationController
       @auction.req_forms.reject! { |c| c.empty? }
       Auction.part_match_or_not_actions(@auction, part_match)
 
-      
+
       Notification.notify_of_opportunities(@auction, @auction.company, "You have a new opportunity to sell!")
       format.html { redirect_to @auction, notice: 'RFQ was successfully created.' }
       format.json { render :show, status: :created, location: @auction }
@@ -93,6 +96,8 @@ class AuctionsController < ApplicationController
 
   def current_opportunities
     @sales_opportunities = AuctionDecorator.decorate_collection(current_user.sales_opportunities)
+    @todays_opportunity_count = 0
+    Auction.find(current_user.sales_opportunities.map(&:id)).each { |x| @todays_opportunity_count +=1 if x.created_at.between?(DateTime.now.at_beginning_of_day.utc, Time.now.utc) }
   end
 
 
@@ -119,9 +124,9 @@ class AuctionsController < ApplicationController
     end
 
     def auction_params
-      params.require(:auction).permit(:company_id, :project_id, :part_num, :target_price, :cycles, :quantity, :destination_company, 
-                                      :destination_address, :destination_zip, :destination_city, :destination_state, 
-                                      :destination_country, :required_date, :resale_status, :resale_yes, :resale_no, 
+      params.require(:auction).permit(:company_id, :project_id, :part_num, :target_price, :cycles, :quantity, :destination_company,
+                                      :destination_address, :destination_zip, :destination_city, :destination_state,
+                                      :destination_country, :required_date, :resale_status, :resale_yes, :resale_no,
                                       condition: [], req_forms: [], invitees: [])
     end
 
