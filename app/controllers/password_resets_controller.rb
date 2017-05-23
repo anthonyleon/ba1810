8b17f6a6adad5d5818698e85f3c3ddde1ac972bd
Anthony Leon <anthonyleon@me.com>
2017-05-23 11:40:05 -0400

    styled reset password view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,31 +1,31 @@
 class PasswordResetsController < ApplicationController
-  skip_before_action :require_logged_in
-  
-  def new
-  end
+	skip_before_action :require_logged_in
 
-  def create
-  	company = Company.find_by_email(params[:email])
-  	if company
-      company.send_password_reset 
-    	redirect_to root_url, :notice => "E-Mail sent with password reset instructions."
-    else
-      redirect_to password_resets_path, :notice => "E-Mail Does Not Exist In Our Database"
-    end
-  end
+	def new
+	end
 
-  def edit
-  	@company = Company.find_by_password_reset_token!(params[:id]) #.find_by(password_reset_token: params[:id])
-  end
+	def create
+		company = Company.find_by_email(params[:email])
+		if company
+			company.send_password_reset
+			redirect_to root_url, :notice => "E-Mail sent with password reset instructions."
+		else
+			redirect_to new_password_reset_path, :notice => "E-Mail Does Not Exist In Our Database"
+		end
+	end
 
-  def update
-  	@company = Company.find_by_password_reset_token!(params[:id])
-  	if @company.password_reset_sent_at < 2.hours.ago
-  		redirect_to new_password_reset_path, :alert => "Password reset has expired."
-  	elsif @company.update_attributes(password: params[:company]["password"])
-  		redirect_to root_url, :notice => "Password has been reset!"
-  	else
-  		render :edit
-  	end  		
-  end
+	def edit
+		@company = Company.find_by_password_reset_token!(params[:id]) #.find_by(password_reset_token: params[:id])
+	end
+
+	def update
+		@company = Company.find_by_password_reset_token!(params[:id])
+		if @company.password_reset_sent_at < 2.hours.ago
+			redirect_to new_password_reset_path, :alert => "Password reset has expired."
+		elsif @company.update_attributes(password: params[:company]["password"])
+			redirect_to root_url, :notice => "Password has been reset!"
+		else
+			render :edit
+		end
+	end
 end
