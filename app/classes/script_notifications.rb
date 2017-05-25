ecec37140f3d317472a2b33b89f4df54dd6742ed
Gabo Martinez <gabmart02@gmail.com>
2017-05-25 15:25:40 -0400

    polishing notification_script

--- app/classes/script_notifications.rb
+++ app/classes/script_notifications.rb
@@ -1,17 +1,22 @@
-buy = ["A new bid was placed in your auction!",
- "A new quote was placed in your RFQ!",
- "You have won an RFQ! Please finalize tax and shipping costs, and input your invoice number.",
- "A new quote was placed in your auction!"]
+class ScriptNotifications
+    def run
+        buy = ["A new bid was placed in your auction!",
+            "A new quote was placed in your RFQ!",
+            "You have won an RFQ! Please finalize tax and shipping costs, and input your invoice number.",
+            "A new quote was placed in your auction!"]
 
-sell = ["You have a new opportunity to sell!",
- "A quote has been placed on an RFQ you are participating in!",
- "A bid has been placed on an auction you are participating in!",
- "A quote has been placed on an auction you are participating in!"]
+        sell = ["You have a new opportunity to sell!",
+            "A quote has been placed on an RFQ you are participating in!",
+            "A bid has been placed on an auction you are participating in!",
+            "A quote has been placed on an auction you are participating in!"]
 
-n = ["You have won an auction! Please finalize tax and shipping costs, and input your invoice number.", "Seller has finalized costs. Please send funds to escrow."]
+        m = ["You have won an auction! Please finalize tax and shipping costs, and input your invoice number.", "Seller has finalized costs. Please send funds to escrow."]
 
-buy.each { |b| Notification.where(message: b).each { |n| n.update(category: :buy) } }
+        buy.each { |b| Notification.where(message: b).each { |n| n.update(category: :buy) } }
 
-sell.each { |b| Notification.where(message: b).each { |n| n.update(category: :sell) } }
+        sell.each { |b| Notification.where(message: b).each { |n| n.update(category: :sell) } }
 
-n.each { |b| Notification.where(message: b).each { |n| n.update(category: :action_required) } }
+        m.each { |b| Notification.where(message: b).each { |n| n.update(category: :action_required) } }
+    end
+    
+end
