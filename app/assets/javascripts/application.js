4182f0900d53a602fa2176e3116b8c4f9f0275a2
Anthony Leon <anthonyleon@me.com>
2017-06-01 14:45:55 -0400

    erfq invites complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,214 +20,214 @@
 
 $(document).ready(function() {
 
-  // makes table rows with "data-link" attribute {"data-link" => "#{whatever_path}"} clickable to that path
-  $("tr[data-link]").click(function() {
-    window.location = $(this).data("link")
-  });
+	// makes table rows with "data-link" attribute {"data-link" => "#{whatever_path}"} clickable to that path
+	/("tr[data-link]").click(function() {
+	/ window.location = $(this).data("link")
+	/);
 
-  var $flashTransition = $('.flash-transition');
-  if (!!$flashTransition.length > 0) {
-    $('.flash-transition').css({
-      'color': 'white',
-      '-webkit-transition': '1.2s',
-      'transition': '1.2s',
-      'height': '40px',
-      'margin': '10px auto'
-    })
-  }
-  setTimeout(function(){
-    $('.flash-transition').height(0);
-    $('.flash-transition').text("");
-  },3500)
+	var $flashTransition = $('.flash-transition');
+	if (!!$flashTransition.length > 0) {
+		$('.flash-transition').css({
+			'color': 'white',
+			'-webkit-transition': '1.2s',
+			'transition': '1.2s',
+			'height': '40px',
+			'margin': '10px auto'
+		})
+	}
+	setTimeout(function(){
+		$('.flash-transition').height(0);
+		$('.flash-transition').text("");
+	},3500)
 })
 
 String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+	return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
 
 $(document).ready(function() {
-  var dataBehavior = function(data, message) {
-    $(data).on("click", function(e){
-      e.preventDefault();
-      var self = $(this);
-      swal({
-        title: "Are you sure?",
-        text: "You will not be able to recover this "+ message + ".",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#DD6B55",
-        confirmButtonText: "Yes, delete it!",
-        cancelButtonText: "No, cancel!",
-        closeOnConfirm: false,
-        closeOnCancel: false,
-      },
-      function(isConfirm){
-        if (isConfirm) {
-          $.ajax({
-            url: $(self).attr("href"),
-            dataType: "JSON",
-            method: "DELETE",
-            success: function() {
-              console.log('yuh');
-              swal("Deleted!", "" + message.capitalize() + " Deleted.", "success");
-              // ge the tr to delete
-              // debugger;
-              self.closest('tr').remove();
-            }
-          });
-        } else {
-          swal("Cancelled", "Your " + message + " was not deleted!", "error");
-        }
-      });
-    });
-  };
+	var dataBehavior = function(data, message) {
+		$(data).on("click", function(e){
+			e.preventDefault();
+			var self = $(this);
+			swal({
+				title: "Are you sure?",
+				text: "You will not be able to recover this "+ message + ".",
+				type: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#DD6B55",
+				confirmButtonText: "Yes, delete it!",
+				cancelButtonText: "No, cancel!",
+				closeOnConfirm: false,
+				closeOnCancel: false,
+			},
+			function(isConfirm){
+				if (isConfirm) {
+					$.ajax({
+						url: $(self).attr("href"),
+						dataType: "JSON",
+						method: "DELETE",
+						success: function() {
+							console.log('yuh');
+							swal("Deleted!", "" + message.capitalize() + " Deleted.", "success");
+							// ge the tr to delete
+							// debugger;
+							self.closest('tr').remove();
+						}
+					});
+				} else {
+					swal("Cancelled", "Your " + message + " was not deleted!", "error");
+				}
+			});
+		});
+	};
 
 // card edit slide up and down functions on auction/destination
-  $('.card-edit').click(function(){
-    $('.card.destination-address').addClass('large');
-      //this method increases the height to 72px
-  });
+	$('.card-edit').click(function(){
+		$('.card.destination-address').addClass('large');
+			//this method increases the height to 72px
+	});
 
-  $('.card-edit-remove').click(function(){
-    $('.card.destination-address.large').removeClass('large');
-      //this method increases the height to 72px
-  });
+	$('.card-edit-remove').click(function(){
+		$('.card.destination-address.large').removeClass('large');
+			//this method increases the height to 72px
+	});
 
-  dataBehavior("[data-inventory-behavior='delete']", "part");
-  dataBehavior("[data-buyer-auctions-behavior='delete']", "auction");
-  dataBehavior("[data-current-opportunities-behavior='delete']", "auction");
-  dataBehavior("[data-auction-bid-behavior='delete']", "bid");
-  dataBehavior("[data-engine-behavior='delete']", "engine");
-  dataBehavior("[data-aircraft-behavior='delete']", "aircraft");
-  dataBehavior("[data-company-purchase-behavior='delete']", "company");
-  dataBehavior("[data-bid-document-behavior='delete'", "bid");
-    dataBehavior("[data-inventory-document-behavior='delete'", "inventory");
+	dataBehavior("[data-inventory-behavior='delete']", "part");
+	dataBehavior("[data-buyer-auctions-behavior='delete']", "auction");
+	dataBehavior("[data-current-opportunities-behavior='delete']", "auction");
+	dataBehavior("[data-auction-bid-behavior='delete']", "bid");
+	dataBehavior("[data-engine-behavior='delete']", "engine");
+	dataBehavior("[data-aircraft-behavior='delete']", "aircraft");
+	dataBehavior("[data-company-purchase-behavior='delete']", "company");
+	dataBehavior("[data-bid-document-behavior='delete'", "bid");
+		dataBehavior("[data-inventory-document-behavior='delete'", "inventory");
 
 $('#engineDTDashBoard').dataTable({
-    "aoColumns": [
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": false }
-    ]
-  });
+		"aoColumns": [
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": false }
+		]
+	});
 
 
-  $('#projectDT').dataTable({
-      "aoColumns": [
-        { "bSortable": true },
-        { "bSortable": true },
-        { "bSortable": true },
-        { "bSortable": true },
-        { "bSortable": true },
-        { "bSortable": true },
-        { "bSortable": true },
-        { "bSortable": false }
-      ]
-    });
-
-
-
-  // $('#buyerAuctionsDT').dataTable({
-  //   "aoColumns": [
-  //     { "bSortable": true },
-  //     { "bSortable": true },
-  //     { "bSortable": true },
-  //     { "bSortable": true },
-  //     { "bSortable": true },
-  //     { "bSortable": true },
-  //     { "bSortable": true },
-  //     { "bSortable": false }
-  //   ],
-  //   "columnDefs": [{ "width": "80px", "targets": 8 }]
-  // });
+	$('#projectDT').dataTable({
+			"aoColumns": [
+				{ "bSortable": true },
+				{ "bSortable": true },
+				{ "bSortable": true },
+				{ "bSortable": true },
+				{ "bSortable": true },
+				{ "bSortable": true },
+				{ "bSortable": true },
+				{ "bSortable": false }
+			]
+		});
 
 
 
-  $('#mybidsDT').dataTable({
-    "aoColumns": [
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": false}
-    ]
-  });
+	// $('#buyerAuctionsDT').dataTable({
+	//   "aoColumns": [
+	//     { "bSortable": true },
+	//     { "bSortable": true },
+	//     { "bSortable": true },
+	//     { "bSortable": true },
+	//     { "bSortable": true },
+	//     { "bSortable": true },
+	//     { "bSortable": true },
+	//     { "bSortable": false }
+	//   ],
+	//   "columnDefs": [{ "width": "80px", "targets": 8 }]
+	// });
+
+
+
+	$('#mybidsDT').dataTable({
+		"aoColumns": [
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": false}
+		]
+	});
 
 
 
 
 
 // ADMIN DATATABLES
-  $('#allInventoryDT').dataTable({
-    "processing": true,
-    "serverSide": true,
-    "ajax": $('#allInventoryDT').data('source'),
-    "pageLength": 50,
-    "aoColumns": [
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true }
-    ]
-  });
+	$('#allInventoryDT').dataTable({
+		"processing": true,
+		"serverSide": true,
+		"ajax": $('#allInventoryDT').data('source'),
+		"pageLength": 50,
+		"aoColumns": [
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true }
+		]
+	});
 
-  $('#matchedAuctionsDT').dataTable({
-    "processing": true,
-    "serverSide": true,
-    "ajax": $('#matchedAuctionsDT').data('source'),
-    "pageLength": 50,
-    "order": [[5, "desc"]],
-    "aoColumns": [
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true }
-    ]
-  });
+	$('#matchedAuctionsDT').dataTable({
+		"processing": true,
+		"serverSide": true,
+		"ajax": $('#matchedAuctionsDT').data('source'),
+		"pageLength": 50,
+		"order": [[5, "desc"]],
+		"aoColumns": [
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true }
+		]
+	});
 
-  $('#allAuctionsDT').dataTable({
-    "pageLength": 50,
-    "processing": true,
-    "serverSide": true,
-    "order": [[5, "desc"]],
-    "ajax": $('#allAuctionsDT').data('source'),
-    "aoColumns": [
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true }
-    ]
-  });
+	$('#allAuctionsDT').dataTable({
+		"pageLength": 50,
+		"processing": true,
+		"serverSide": true,
+		"order": [[5, "desc"]],
+		"ajax": $('#allAuctionsDT').data('source'),
+		"aoColumns": [
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true }
+		]
+	});
 
-  $('#unmatchedAuctionsDT').dataTable({
-    "processing": true,
-    "serverSide": true,
-    "ajax": $('#unmatchedAuctionsDT').data('source'),
-    "pageLength": 50,
-    "order": [[5, "desc"]],
-    "aoColumns": [
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true },
-      { "bSortable": true }
-    ]
-  });
+	$('#unmatchedAuctionsDT').dataTable({
+		"processing": true,
+		"serverSide": true,
+		"ajax": $('#unmatchedAuctionsDT').data('source'),
+		"pageLength": 50,
+		"order": [[5, "desc"]],
+		"aoColumns": [
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true },
+			{ "bSortable": true }
+		]
+	});
 
 });
 
