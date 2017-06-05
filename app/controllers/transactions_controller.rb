9397d2183d4d536d60a7838315f00fe08bf30326
Gabo Martinez <gabmart02@gmail.com>
2017-06-05 12:19:32 -0400

    fixes transactions create mailer

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -57,7 +57,7 @@ class TransactionsController < ApplicationController
 		@transaction.destination = @destination
 		@auction.update(active: false) if @auction.active
 		CompanyMailer.won_auction_notification(@transaction.bid, @transaction.seller, @transaction).deliver_later(wait_until: 1.minute.from_now) &&
-			Notification.notify(@bid, @bid.seller, "You have won an RFQ! Please finalize tax and shipping costs, and input your invoice number.",
+			Notification.notify(@transaction.bid, @transaction.seller, "You have won an RFQ! Please finalize tax and shipping costs, and input your invoice number.",
 				transaction: @transaction) unless Notification.exists?(@bid, "You have won an RFQ! Please finalize tax and shipping costs, and input your invoice number.")
 		respond_to do |format|
 			if @transaction.update(transaction_params)
