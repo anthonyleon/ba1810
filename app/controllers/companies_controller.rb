9b4de0ec8d9a361560e59c007213359d05662b3e
Gabo Martinez <gabmart02@gmail.com>
2017-06-29 17:37:24 -0400

    takes password out of strong params in companies_controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -121,8 +121,8 @@ class CompaniesController < ApplicationController
 		end
 
 		def company_params
-			params.require(:company).permit(:name, :armor_account_id, :armor_user_id, :email, :ein, :password,
-				:password_confirmation, :representative, :phone, :address, :city, :state, :zip, :country,
+			params.require(:company).permit(:name, :armor_account_id, :armor_user_id, :email, :ein, 
+				:representative, :phone, :address, :city, :state, :zip, :country,
 				:inc_country, :inc_state, :business_type, :url)
 		end
 
